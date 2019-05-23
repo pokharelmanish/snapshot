56a0aaa24c8abf9258bef323f811e7670afcad40
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-05-23 13:17:48 -0700

    update comment in person_search_by_last_first_name_query_builder;

--- app/services/person_search_by_last_first_name_query_builder_part_one.rb
+++ app/services/person_search_by_last_first_name_query_builder_part_one.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# PersonSearchByNameQueryBuilder is a service class responsible for creation
+# PersonSearchByLastFirstNameQueryBuilder is a service class responsible for creation
 # of an elastic search person search query
 module PersonSearchByLastFirstNameQueryBuilderPartOne
   attr_reader :is_client_only, :last_name, :first_name, :middle_name, :suffix
