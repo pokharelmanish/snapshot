ff7682c5990fbbe4d7ddedd0baa037e1a0ba469a
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-05-24 11:56:01 -0700

    update last middle first name query spec;

--- spec/services/person_search_by_last_middle_first_name_query_builder_part_one_spec.rb
+++ spec/services/person_search_by_last_middle_first_name_query_builder_part_one_spec.rb
@@ -45,7 +45,7 @@ describe PersonSearchByLastMiddleFirstNameQueryBuilderPartOne do
     end
 
     context 'returns hash' do
-      it 'with full name query' do
+      it 'with last, middle, and first name with suffix' do
         query_builder = QueryBuilder.new(person_search_fields: last_middle_first_name_with_suffix_params)
         query = query_builder.extend(described_class).query
         expect(query.as_json).to eq last_middle_first_name_with_suffix['query']
