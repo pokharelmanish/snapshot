98978a3506c4661772d6b2e869408c236e4328f3
Ed Gibbs <edward_gibbs@yahoo.com>
2019-06-03 11:47:43 -0700

    Fixes reliance on erb in javascript file :unicorn:

---
+++ app/javascript/common/url_helper.js
@@ -0,0 +1,4 @@
+export const urlHelper = path => {
+  const railsRelativeUrlPath = process.env.RAILS_RELATIVE_URL_ROOT ? process.env.RAILS_RELATIVE_URL_ROOT : '';
+  return railsRelativeUrlPath + path;
+};
