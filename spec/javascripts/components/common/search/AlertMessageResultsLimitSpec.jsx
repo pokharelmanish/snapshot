d8fdb348ebd977702117e7a991b76bc7795754ed
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-06-03 15:21:56 -0700

    Add handleCloseAlert callback to close the AlertErrorMessage;

--- spec/javascripts/components/common/search/AlertMessageResultsLimitSpec.jsx
+++ spec/javascripts/components/common/search/AlertMessageResultsLimitSpec.jsx
@@ -18,21 +18,49 @@ describe('AlertMessageResultsLimit', () => {
       expect(container.exists()).toBe(true)
     })
 
-    it('does not render an AlertErrorMessage when results are 250 or less', () => {
-      const component = render({total: 250})
-      const alertMessage = component.find('AlertErrorMessage')
-      expect(alertMessage.exists()).toBe(false)
-    })
+    describe('AlertErrorMessage', () => {
+      describe('when the total results are greater than 250', () => {
+        describe('and closeAlert is false', () => {
+          it('renders an AlertErrorMessage', () => {
+            const alertMessage = component.find('AlertErrorMessage')
+            expect(alertMessage.exists()).toBe(true)
+          })
+        })
 
-    it('renders an AlertErrorMessage when results are 250 or more', () => {
-      const alertMessage = component.find('AlertErrorMessage')
-      expect(alertMessage.exists()).toBe(true)
+        describe('and closeAlert is true', () => {
+          it('does not render an AlertErrorMessage', () => {
+            component.setState({closeAlert: true})
+            const alertMessage = component.find('AlertErrorMessage')
+            expect(alertMessage.exists()).toBe(false)
+          })
+        })
+      })
+
+      describe('when the total results are 250 or less', () => {
+        it('does not render an AlertErrorMessage', () => {
+          const component = render({total: 250})
+          const alertMessage = component.find('AlertErrorMessage')
+          expect(alertMessage.exists()).toBe(false)
+        })
+      })
     })
 
     it('sets the correct message for the alert', () => {
       const message = 'Over 250 search results have been found. If possible, please refine your search by adding additional search criteria, then click the "Search" button again.'
       const props = component.find('AlertErrorMessage').props()
       expect(props.message).toEqual(message)
+    })
+  })
+
+  describe('callbacks', () => {
+    describe('handleCloseAlert', () => {
+      it('sets the closeAlert state to true', () => {
+        const component = render({total: 251})
+        expect(component.state().closeAlert).toBe(false)
+        const alertErrorMessage = component.find('AlertErrorMessage')
+        alertErrorMessage.props().closeAlert()
+        expect(component.state().closeAlert).toBe(true)
+      })
     })
   })
 })
