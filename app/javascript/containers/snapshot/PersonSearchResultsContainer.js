b73ce27879292f04ddbdb231845cd340d8a9f297
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-06-04 15:39:20 -0700

    Add totalResultsReceived arg to onLoadMoreResults dispatcher in PersonSearchResultsContainer;

--- app/javascript/containers/snapshot/PersonSearchResultsContainer.js
+++ app/javascript/containers/snapshot/PersonSearchResultsContainer.js
@@ -22,8 +22,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  const onLoadMoreResults = (personSearchFields) => {
-    dispatch(loadMoreResults(true, true, personSearchFields))
+  const onLoadMoreResults = (personSearchFields, totalResultsReceived) => {
+    dispatch(loadMoreResults(true, true, personSearchFields, totalResultsReceived))
   }
   const setCurrentPageNumber = (pageNumber) => {
     dispatch(setSearchCurrentPage(pageNumber))
