02083ff928e48b8a77ca074471ece49eec242cf8
Ed Gibbs <edward_gibbs@yahoo.com>
2019-06-04 14:20:48 -0700

    Removes extra semis :unicorn:

--- app/javascript/common/url_helper.js
+++ app/javascript/common/url_helper.js
@@ -1,4 +1,4 @@
 export const urlHelper = path => {
-  const railsRelativeUrlPath = process.env.RAILS_RELATIVE_URL_ROOT ? process.env.RAILS_RELATIVE_URL_ROOT : '';
-  return railsRelativeUrlPath + path;
-};
+  const railsRelativeUrlPath = process.env.RAILS_RELATIVE_URL_ROOT ? process.env.RAILS_RELATIVE_URL_ROOT : ''
+  return railsRelativeUrlPath + path
+}
