77d4679453923e272950f5a418c1958e058f1a8e
Ed Gibbs <edward_gibbs@yahoo.com>
2019-06-07 16:22:44 -0700

    Removes all the views and containers for people related things :unicorn:

--- app/javascript/screenings/PersonCardView.jsx
+++ app/javascript/screenings/PersonCardView.jsx
@@ -1,15 +1,8 @@
 import PersonCardContainer from 'containers/screenings/PersonCardContainer'
-import PersonCSECFormContainer from 'containers/screenings/PersonCSECFormContainer'
-import PersonDemographicsFormContainer from 'containers/screenings/PersonDemographicsFormContainer'
-import PersonRaceFormContainer from 'containers/screenings/PersonRaceFormContainer'
-import PersonEthnicityFormContainer from 'containers/screenings/PersonEthnicityFormContainer'
 import PersonPhoneNumbersContainer from 'containers/screenings/PersonPhoneNumbersContainer'
-import PersonPhoneNumbersFormContainer from 'containers/screenings/PersonPhoneNumbersFormContainer'
 import PersonShowContainer from 'containers/screenings/PersonShowContainer'
 import PersonAddressesContainer from 'containers/screenings/PersonAddressesContainer'
 import PersonReadOnlyAddressesContainer from 'containers/screenings/PersonReadOnlyAddressesContainer'
-import PersonAddressesFormContainer from 'containers/screenings/PersonAddressesFormContainer'
-import PersonFormContainer from 'containers/screenings/PersonFormContainer'
 import PropTypes from 'prop-types'
 import React from 'react'
 
@@ -18,21 +11,12 @@ const PersonCardView = ({personId}) => (
     personId={personId}
     edit={
       <div>
-        <PersonFormContainer personId={personId} />
-        <PersonDemographicsFormContainer personId={personId} />
-        <PersonRaceFormContainer personId={personId} />
-        <PersonEthnicityFormContainer personId={personId} />
-        <PersonPhoneNumbersFormContainer personId={personId} />
         <PersonReadOnlyAddressesContainer personId={personId} />
-        <PersonAddressesFormContainer personId={personId} />
-        <PersonCSECFormContainer personId={personId} />
       </div>
     }
     show={
       <div>
         <PersonShowContainer personId={personId} />
-        <PersonPhoneNumbersContainer personId={personId} />
-        <PersonAddressesContainer personId={personId} />
       </div>
     }
   />
