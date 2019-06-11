f98f10dc90a0bfbcaf5f27f01d4fc5e32f5ed386
Ed Gibbs <edward_gibbs@yahoo.com>
2019-06-11 11:46:46 -0700

    Removes the last of screening containers :unicorn:

--- app/javascript/containers/screenings/PersonAddressesContainer.js
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux'
-import AddressesShow from 'views/people/AddressesShow'
-import {selectFormattedAddresses} from 'selectors/participantSelectors'
-
-const mapStateToProps = (state, {personId}) => (
-  {addresses: selectFormattedAddresses(state, personId).toJS()}
-)
-
-export default connect(mapStateToProps)(AddressesShow)
--- app/javascript/containers/screenings/PersonPhoneNumbersContainer.js
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux'
-import PhoneNumbersShow from 'views/people/PhoneNumbersShow'
-import {getPersonFormattedPhoneNumbersSelector} from 'selectors/screening/personShowSelectors'
-
-const mapStateToProps = (state, {personId}) => ({
-  phoneNumbers: getPersonFormattedPhoneNumbersSelector(state, personId).toJS(),
-})
-
-export default connect(mapStateToProps)(PhoneNumbersShow)
