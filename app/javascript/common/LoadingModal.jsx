2a58ea929ed63c7a19e7ef42d1f6d19b55b4b26e
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-06-19 17:08:20 -0700

    Add LoadingModal component;

---
+++ app/javascript/common/LoadingModal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {ModalComponent} from 'react-wood-duck'
+
+const renderModalBody = () => (
+  <div className='client-detail-loading-icon'>
+    <i className='fa fa-spinner fa-spin-faster' />
+  </div>
+)
+
+const LoadingModal = ({show, size, title}) => {
+  return (
+    <ModalComponent
+      showModal={show}
+      modalBody={renderModalBody()}
+      modalSize={size}
+      modalTitle={title}
+    />
+  )
+}
+
+LoadingModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  size: PropTypes.string.isRequired,
+  title: PropTypes.string,
+}
+
+export default LoadingModal
