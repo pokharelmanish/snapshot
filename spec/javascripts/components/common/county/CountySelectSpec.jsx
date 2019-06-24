ea9a938ccb9d2e2f63f63ead90ec388756d2c3e9
Gustavo Gomez <ggomez@cambriasolutions.com>
2019-06-24 15:05:20 -0700

    add onKeyPress to CountySelect;

--- spec/javascripts/components/common/county/CountySelectSpec.jsx
+++ spec/javascripts/components/common/county/CountySelectSpec.jsx
@@ -1,25 +1,27 @@
-import CountySelect from 'common/county/CountySelect'
 import React from 'react'
 import {shallow} from 'enzyme'
+import CountySelect from 'common/county/CountySelect'
+
+const render = ({
+  counties = [],
+  gridClassName = 'gridClassName',
+  id = 'county-select',
+  onChange = () => null,
+  onKeyPress = () => null,
+  value = null,
+}) => {
+  const props = {
+    counties,
+    gridClassName,
+    id,
+    onChange,
+    onKeyPress,
+    value,
+  }
+  return shallow(<CountySelect {...props} />)
+}
 
 describe('CountySelect', () => {
-  function render({
-    counties = [],
-    gridClassName = 'gridClassName',
-    id = 'county-select',
-    onChange = () => null,
-    value = null,
-  }) {
-    const props = {
-      counties,
-      gridClassName,
-      id,
-      onChange,
-      value,
-    }
-    return shallow(<CountySelect {...props} />)
-  }
-
   it('displays the select field', () => {
     const component = render({counties: [
       {code: '123', value: 'San Francisco'},
@@ -34,7 +36,8 @@ describe('CountySelect', () => {
       {code: '456', value: 'Sacramento'},
     ],
     value: 'San Francisco'})
-    expect(component.find('SelectField').props().value).toEqual('San Francisco')
+    const selectField = component.find('SelectField')
+    expect(selectField.props().value).toEqual('San Francisco')
   })
 
   it('passes id, and gridClassName to select field', () => {
@@ -42,11 +45,13 @@ describe('CountySelect', () => {
       gridClassName: 'my-class-name',
       id: 'my-id',
     })
-    expect(component.find('SelectField').props().gridClassName).toEqual('my-class-name')
-    expect(component.find('SelectField').props().id).toEqual('my-id')
+    const selectField = component.find('SelectField')
+    const selectFieldProps = selectField.props()
+    expect(selectFieldProps.gridClassName).toEqual('my-class-name')
+    expect(selectFieldProps.id).toEqual('my-id')
   })
 
-  it('calls back with the full system code when selection changes', () => {
+  it('calls onChange with the full system code when selection changes', () => {
     const onChange = jasmine.createSpy('onChange')
     const component = render({
       counties: [
@@ -56,13 +61,12 @@ describe('CountySelect', () => {
       onChange,
       value: 'Sacramento',
     })
-
-    component.find('SelectField').props().onChange({target: {value: 'San Francisco'}})
-
+    const selectField = component.find('SelectField')
+    selectField.props().onChange({target: {value: 'San Francisco'}})
     expect(onChange).toHaveBeenCalledWith({code: '123', value: 'San Francisco'})
   })
 
-  it('calls back with the null when selection changes to unknown code', () => {
+  it('calls onChange with the null when selection changes to unknown code', () => {
     const onChange = jasmine.createSpy('onChange')
     const component = render({
       counties: [
@@ -72,9 +76,17 @@ describe('CountySelect', () => {
       onChange,
       value: 'Sacramento',
     })
-
-    component.find('SelectField').props().onChange({target: {value: 'Merrimack'}})
-
+    const selectField = component.find('SelectField')
+    selectField.props().onChange({target: {value: 'Merrimack'}})
     expect(onChange).toHaveBeenCalledWith(null)
+  })
+
+  it('calls onKeyPress when a user presses a key', () => {
+    const event = {charCode: 13}
+    const onKeyPress = jasmine.createSpy('onKeyPress')
+    const component = render({onKeyPress})
+    const selectField = component.find('SelectField')
+    selectField.simulate('keypress', event)
+    expect(onKeyPress).toHaveBeenCalledWith(event)
   })
 })
