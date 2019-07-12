08e05ff34222affd78c41b4b9f940efecd009628
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-07-12 11:49:07 -0700

    SNAP-1120 remove unneeded person search result props

--- app/javascript/containers/snapshot/PersonSearchResultsContainer.js
+++ app/javascript/containers/snapshot/PersonSearchResultsContainer.js
@@ -20,8 +20,6 @@ const mapStateToProps = state => {
     results: selectPeopleResults(state).toJS(),
     total: selectResultsTotalValue(state),
     personSearchFields: selectPersonSearchFields(state),
-    resultsSubset: selectSearchResultsSubset(state),
-    currentRow: selectSearchResultsCurrentRow(state),
     isSearchResults: selectCheckSearchResults(state),
   }
 }
@@ -30,10 +28,8 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   const onLoadMoreResults = (personSearchFields, totalResultsReceived, totalResultsRequested) => {
     dispatch(loadMoreResults(true, true, personSearchFields, totalResultsReceived, totalResultsRequested))
   }
-  const setCurrentPageNumber = (pageNumber) => dispatch(setSearchCurrentPage(pageNumber))
-  const setCurrentRowNumber = (pageNumber) => dispatch(setSearchCurrentRow(pageNumber))
   const onAuthorize = (id) => dispatch(authorizeSnapshotPerson(id))
-  return {onLoadMoreResults, setCurrentPageNumber, setCurrentRowNumber, onAuthorize, dispatch}
+  return {onAuthorize, dispatch}
 }
 
 export default connect(
