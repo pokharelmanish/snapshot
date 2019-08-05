8d479465b8e55a30434b1b3317a991973bf2e6fd
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-08-05 11:25:36 -0700

    CMO-14 add client ID validation

--- app/javascript/views/advancedSearch/AdvancedSearchValidationSchema.jsx
+++ app/javascript/views/advancedSearch/AdvancedSearchValidationSchema.jsx
@@ -1,6 +1,8 @@
 import * as Yup from 'yup'
 
 const validationSchema = Yup.object().shape({
+  clientId: Yup.string()
+    .matches(/\d{4}-\d{4}-\d{4}-\d{7}/, {message: 'Client ID number must be 19 digits long.', excludeEmptyString: true}),
   ssn: Yup.string()
     .matches(/\d{3}-\d{2}-\d{4}/, {message: 'Social security number must be 9 digits long.', excludeEmptyString: true})
     .matches(/(?!0{3})[\d_]{3}-(?!0{2})[\d_]{0,2}-(?!0{4})[\d_]{0,4}/, {message: 'Social security number cannot contain all 0s in a group.', excludeEmptyString: true}),
